5fcbf37ad64442f6313675841454f393546a6d8c
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-20 08:45:07 -0500

    add createCube() and createCube2() methods

---
+++ src/MultidimensionalArrays/Main.java
@@ -0,0 +1,9 @@
+package MultidimensionalArrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][][] cube = MultidimensionalArrays.createCube2();
+        MultidimensionalArrays.printThreeDimArray(cube);
+    }
+}
--- src/MultidimensionalArrays/MultidimensionalArrays.java
+++ src/MultidimensionalArrays/MultidimensionalArrays.java
@@ -20,7 +20,7 @@ public class MultidimensionalArrays {
         System.out.println();
 //        System.out.println(isSymmetric(twoDimArray) ? "YES" : "NO");
 
-        printTheThirdRow(twoDimArray);
+//        printTheThirdRow(twoDimArray);
 
     }
 
@@ -36,18 +36,68 @@ public class MultidimensionalArrays {
         return true;
     }
 
-     static void printTwoDimArray(int[][] array) {
+
+    /*
+    Create a method that prints the third row of a two dimensional array. Assume there are always at least three rows.
+     */
+
+    public static void printTheThirdRow(int[][] twoDimArray) {
+        for (int j : twoDimArray[2]) {
+            System.out.print(j + " ");
+        }
+    }
+
+    /*
+    You need to implement the createCube method. It should create a 3x3x3 three-dimensional array with the following content:
+
+    [0, 1, 2]	[3, 4, 5]	[6, 7, 8]
+    [0, 1, 2]	[3, 4, 5]	[6, 7, 8]
+    [0, 1, 2]	[3, 4, 5]	[6, 7, 8]
+
+    The elements should be of type int.
+     */
+
+    static int[][][] createCube() {
+        final int n = 3;
+        int[][][] cube = new int[n][n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    cube[i][j][k] = k + (n * j);
+                }
+            }
+        }
+
+        return cube;
+    }
+
+    static int[][][] createCube2() {
+        int[][] row = {{0, 1, 2}, {3, 4, 5}, {6, 7,  8}};
+        int[][][] cube = new int[][][] {row, row, row};
+
+        return cube;
+    }
+
+    static void printTwoDimArray(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
-     }
+    }
 
-    public static void printTheThirdRow(int[][] twoDimArray) {
-        for (int j : twoDimArray[2]) {
-            System.out.print(j + " ");
+    static void printThreeDimArray(int[][][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print("[");
+                for (int k = 0; k < array[i][j].length; k++) {
+                    System.out.print(array[i][j][k] + " ");
+                }
+                System.out.print("], ");
+            }
+            System.out.println();
         }
     }
 
