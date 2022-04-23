15770c454e44dfde64172182b06ea18879a6d064
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-22 21:10:53 -0500

    create CinemaRoom static class for stage implementation 3/5

--- src/com/ernesto/Main.java
+++ src/com/ernesto/Main.java
@@ -6,6 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = scanner.nextInt();
+
+        CinemaRoom cinemaRoom = new CinemaRoom(rows, seats);
+
+        cinemaRoom.printCinemaRoom();
+
+        cinemaRoom.selectSeat(scanner);
+
+    }
+
+    static void incomeCalculator() {
+        Scanner scanner = new Scanner(System.in);
         final int seatPrice = 10;
 
         System.out.println("Enter the number of rows: ");
@@ -22,18 +38,112 @@ public class Main {
         }
 
         System.out.println("Total income: \n$" + income);
-
     }
 
-    static void printCinemaRoom() {
-        System.out.println("Cinema:\n" +
-                "  1 2 3 4 5 6 7 8\n" +
-                "1 S S S S S S S S\n" +
-                "2 S S S S S S S S\n" +
-                "3 S S S S S S S S\n" +
-                "4 S S S S S S S S\n" +
-                "5 S S S S S S S S\n" +
-                "6 S S S S S S S S\n" +
-                "7 S S S S S S S S");
+    enum roomType {
+        NORMAL,
+        BIG
+    }
+
+    static class CinemaRoom {
+        int rows;
+        int seats;
+        String[][] room;
+        roomType type;
+
+        CinemaRoom(int rows, int seats) {
+            this.rows = rows;
+            this.seats = seats;
+            this.room = new String[this.rows][this.seats];
+            type = rows * seats > 60 ? roomType.BIG : roomType.NORMAL;
+            populateCinemaRoom();
+        }
+
+        public int getRows() {
+            return rows;
+        }
+
+        public int getSeats() {
+            return seats;
+        }
+
+        public String[][] getRoom() {
+            return room;
+        }
+
+        public void setRows(int rows) {
+            this.rows = rows;
+        }
+
+        public void setSeats(int seats) {
+            this.seats = seats;
+        }
+
+        public void populateCinemaRoom() {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < seats; j++) {
+                    room[i][j] = "S";
+                }
+            }
+        }
+
+        public void printCinemaRoom() {
+            // Print Header
+            System.out.println("\nCinema: ");
+            System.out.print("  ");
+            for (int i = 1; i <= seats; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+            // Print body
+            for (int i = 0; i < room.length; i++) {
+                System.out.print((i + 1) + " ");
+                for (int j = 0; j < room[i].length; j++) {
+                    System.out.print(room[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        public void selectSeat(Scanner scanner) {
+            System.out.println("\nEnter a row number:");
+            int selectedRow = scanner.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            int selectedSeat = scanner.nextInt();
+
+            room[selectedRow - 1][selectedSeat - 1] = "B";
+
+            int ticketPrice = 10;
+
+            if (type == roomType.BIG && selectedRow > rows / 2) {
+                ticketPrice = 8;
+            }
+
+            System.out.println("\nTicket price: $" + ticketPrice);
+
+            printCinemaRoom();
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
